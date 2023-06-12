08dff470a74ac950668a7e517048edb71d656a9b
morqva <87177473+morqva@users.noreply.github.com>
2023-06-12 11:12:52 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть шлях до каталогу: ");
+        string directoryPath = Console.ReadLine(); 
+
+        if (!Directory.Exists(directoryPath))
+        {
+            Console.WriteLine("Каталог не існує.");
+            return;
+        }
+
+        Console.Write("Введіть шаблон файлів (наприклад, *.txt): ");
+        string filePattern = Console.ReadLine(); 
+
+        string[] files = Directory.GetFiles(directoryPath, filePattern);
+
+        foreach (string filePath in files)
+        {
+            try
+            {
+                FileAttributes attributes = File.GetAttributes(filePath);
+                attributes &= ~FileAttributes.Hidden; 
+                File.SetAttributes(filePath, attributes);
+
+                Console.WriteLine("Атрибути файлу {0} було змінено.", filePath);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Помилка: {0}", ex.Message);
+            }
+        }
+    }
+}
